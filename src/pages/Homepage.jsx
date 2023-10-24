44c48f6d2a9f8563faeef99242b528651bc5963a
Bhone <sherlockbhone999@gmail.com>
2023-10-25 06:20:02 +0630

    11111

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,9 @@
-
+import { useNavigate } from "react-router-dom"
 
 export default function Home () {
-  return <div>Home page</div>
+  const navigate = useNavigate()
+  return <div class="text-red-500">
+  Home page
+  <button onClick={()=>navigate('/camera')}>to camera</button>
+  </div>
 }
