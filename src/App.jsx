37ca065084ebb048ebe646e604fe3c3487042cf2
Bhone <sherlockbhone999@gmail.com>
2023-10-24 21:15:10 +0630

    1111

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import { BrowserRouter, Routes, Route, useParams } from "react-router-dom"
 
 function App() {
   //peerId of client that send the stream
-  const peerId = useParams()
-  const nameOfStreamProvider = useParams()
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
-        <Route path="/view/:nameOfStreamProvider" element={<ViewerPage />} /> 
-        <Route path="/camera/:peerId" element={ <CameraPage /> } />
+        <Route path="/view" element={<ViewerPage />} /> 
+        <Route path="/camera" element={ <CameraPage /> } />
       </Routes>
     </BrowserRouter>
   )
