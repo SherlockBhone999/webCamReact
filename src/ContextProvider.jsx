342930e91fbae6e72695df581828379d6d5bfcde
Bhone <sherlockbhone999@gmail.com>
2023-10-27 16:39:37 +0630

    vd1

--- src/ContextProvider.jsx
+++ src/ContextProvider.jsx
@@ -69,6 +69,8 @@ const ContextProvider = () => {
       users,
       isViewer,
       setIsViewer,
+      peerConnectionRef,
+      peer,
 
     }}>
       <App />
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom"
+
 import { useRef , useEffect } from "react"
 
 import { useContext } from "react"
@@ -6,31 +6,105 @@ import { Context } from "../ContextProvider"
 
 //accept one call 
 export default function Viewer () {
-  
+  const { peer } = useContext(Context)
   //we don't need peerId to accept the call, just to know who provide the stream
-  const videoRef = useRef()
+  const videoRef = useRef(null)
 
-  
-  const streamVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video : {facingMode : "user"}})
-    .then(stream => {
-      videoRef.current.srcObject = stream
-      videoRef.current.play()
-    })
-  }
-  
   useEffect(()=>{
-    streamVideo()
+    peer.on('call', (call) => {
+      call.on("stream", (remoteStream) => {
+        videoRef.current.srcObject = remoteStream;
+        videoRef.current.play();
+      })
+      
+      call.on('close', () => {
+        videoRef.current.remove()
+      })
+      
+    })
   },[])
 
   return (
     <div>
       ViewerPage
-      {/*
+  
       <div>
         <video ref={videoRef} style={{ width : 90 }}/>
       </div>
-    */}
+    
     </div>
   )
 }
+
+
+
+/*
+import { useEffect, useRef, useState } from 'react';
+import Peer from 'peerjs';
+import './App.css';
+
+function App() {
+  const [peerId, setPeerId] = useState('');
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on('open', (id) => {
+      setPeerId(id)
+    });
+
+    peer.on('call', (call) => {
+      var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+        call.answer(mediaStream)
+        call.on('stream', function(remoteStream) {
+          remoteVideoRef.current.srcObject = remoteStream
+          remoteVideoRef.current.play();
+        });
+      });
+    })
+
+    peerInstance.current = peer;
+  }, [])
+
+  const call = (remotePeerId) => {
+    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
+
+      const call = peerInstance.current.call(remotePeerId, mediaStream)
+
+      call.on('stream', (remoteStream) => {
+        remoteVideoRef.current.srcObject = remoteStream
+        remoteVideoRef.current.play();
+      });
+    });
+  }
+
+  return (
+    <div className="App">
+      <h1>Current user id is {peerId}</h1>
+      <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      <div>
+        <video ref={currentUserVideoRef} />
+      </div>
+      <div>
+        <video ref={remoteVideoRef} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+*/
